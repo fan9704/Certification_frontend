a0878585f7352041e68f4f682de3104ddbfc9c86
fan9704 <cxz123499@gmail.com>
2022-03-17 16:15:52 +0800

    Update vue.config Change target

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8000', //API服务器的地址
+                target: 'https://www.server-fan9704.eu.org',
                 secure: true,
                 ws: true, //代理websockets
                 changeOrigin: true,
